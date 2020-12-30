4baa5af9e66a2dc7da5c422af19b5c6bf70264b7
Nazar Taran <ntaran@c1r6p11.khpi.ucode-connect.study>
2020-12-30 20:21:55 +0200

    30.12

---
+++ libmx/inc/libmx.h
@@ -0,0 +1,4 @@
+#pragma once
+
+#include "libmxinc.h"
+#include "libmxfunc.h"
---
+++ libmx/inc/libmxinc.h
@@ -0,0 +1,7 @@
+#include "libmx.h"
+
+#include <fcntl.h>
+#include <malloc/malloc.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <unistd.h>
